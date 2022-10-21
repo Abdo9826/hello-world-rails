c864fcc73b0a8e68b54a0d6cc58755e5fa4a77c0
unknown <abodyalex1@gmail.com>
2022-10-22 02:06:28 +0300

    add some changes for another review

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(massege: 'Hello, world!')
+Greeting.create(massege: 'Hello, Develpers!')
+Greeting.create(massege: 'Hello, Rails!')
+Greeting.create(massege: 'Hello, React!')
+Greeting.create(massege: 'Hello, Redux!')
+Greeting.create(massege: 'Hello, JavaScript!')
